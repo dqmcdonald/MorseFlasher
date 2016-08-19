bba66b4fc6d982b9448e10fb121d9654460bc969
Quentin McDonald <Quentin.McDonald@schrodinger.com>
2016-08-20 07:53:36 +1200

    Some code reorganization

--- MorseFlasher.ino
+++ MorseFlasher.ino
@@ -11,24 +11,27 @@
    pin A3 (chip pin 2)
 
    Between each set of flashes the ATTiny85 will sleep
-   for 15 seconds.
+   for 15 seconds in order to maximise battery list
 
    June 2016
    D Q McDonald
  *****************************************/
 
-#include <ctype.h>
+// This is the text which will be flashed
+// repeatedly. Characters only, no numbers
+// are supported. Max is 30 characters, any more will be ignored.
+const char* text = "SOS";
 
+
+#include <ctype.h>
 #include <avr/io.h>        // Adds useful constants
 #include <util/delay.h>    // Adds delay_ms and delay_us functions
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
 
-
 // Variables for the Sleep/power down modes:
 volatile int watchdog_counter = 0;
 #define WATCHDOG_MAX 15
-
 
 // Routines to set and clear bits (used in the sleep code)
 #ifndef cbi
@@ -38,7 +41,7 @@ volatile int watchdog_counter = 0;
 #define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
 #endif
 
-
+// Pin definitions
 #define LED_PIN 0
 #define ANALOG_PIN A3
 
@@ -47,15 +50,20 @@ volatile int watchdog_counter = 0;
 #define DOT 0
 #define DASH 1
 #define SPACE_CODE 64
+#define LOW_BIT_MASK 0b00001111
 
-// How long the shortest flash is:
+// For setting up the watchdog timer:
+//0=16ms, 1=32ms,2=64ms,3=128ms,4=250ms,5=500ms
+// 6=1 sec,7=2 sec, 8=4 sec, 9= 8sec
+enum class SleepDuration  { SLEEP16MS = 0, SLEEP32MS = 1, SLEEP128MS = 3, SLEEP250MS = 4,
+                            SLEEP500MS = 5, SLEEP1S = 6, SLEEP2S = 7, SLEEP4S = 8, SLEEP8S = 9
+                          };
+
+
+// How long the shortest flash is. Will will be set by reading the voltage from pin2
 int dot_duration = 200   ;
 
-// This is the text which will be flashed
-// repeatedly. Characters only, no numbers
-// are supported. Max is 30 characters, any more will be ignored.
-const char* text = "SOS";
-
+// The maximum number of characters we can support:
 #define MAX_CHARS 30
 
 int num_characters = 0;
@@ -65,6 +73,94 @@ int num_characters = 0;
 // represents the length of the code (1-4 for
 // characters),
 byte morse[MAX_CHARS];
+
+void setup() {
+
+  convert_text_to_morse();
+  
+  pinMode( LED_PIN, OUTPUT );
+  pinMode( ANALOG_PIN, INPUT );
+
+  // Flash pins on startup
+  digitalWrite(LED_PIN, LOW);
+  delay(100);
+  digitalWrite(LED_PIN, HIGH);
+  delay(50);
+  digitalWrite(LED_PIN, LOW );
+  delay(100);
+  digitalWrite(LED_PIN, HIGH);
+  delay(50);
+  digitalWrite(LED_PIN, LOW);
+  delay(100);
+  digitalWrite(LED_PIN, HIGH);
+  delay(50);
+  digitalWrite(LED_PIN, LOW);
+  delay(50);
+  digitalWrite(LED_PIN, HIGH);
+  delay(100);
+  digitalWrite(LED_PIN, LOW);
+  delay(3000);
+
+  setup_watchdog(SleepDuration::SLEEP1S); // Set the watchdog for 1s
+
+}
+
+void loop () {
+
+  if ( watchdog_counter > WATCHDOG_MAX )
+  { // Only proceed if we've slept enough times:
+    
+    watchdog_counter = 0;
+    flash_morse();
+    
+  }
+
+  // Shut down the ATTiny as much as possible:
+  system_sleep();
+
+}
+
+
+// 0=16ms, 1=32ms,2=64ms,3=128ms,4=250ms,5=500ms
+// 6=1 sec,7=2 sec, 8=4 sec, 9= 8sec
+void setup_watchdog(SleepDuration sleep_duration) {
+
+  int ii = static_cast<int>(sleep_duration);
+  byte bb;
+  int ww;
+  if (ii > 9 ) ii = 9;
+  bb = ii & 7;
+  if (ii > 7) bb |= (1 << 5);
+  bb |= (1 << WDCE);
+  ww = bb;
+
+  MCUSR &= ~(1 << WDRF);
+  // start timed sequence
+  WDTCR |= (1 << WDCE) | (1 << WDE);
+  // set new watchdog timeout value
+  WDTCR = bb;
+  WDTCR |= _BV(WDIE);
+}
+
+// Watchdog Interrupt Service / is executed when watchdog timed out
+ISR(WDT_vect) {
+  watchdog_counter++; // increment the global counter
+}
+
+
+// set system into the sleep state
+// system wakes up when wtchdog is timed out
+void system_sleep() {
+
+  cbi(ADCSRA, ADEN);                   // switch Analog to Digitalconverter OFF
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN); // sleep mode is set here
+  sleep_enable();
+  sleep_mode();                        // System actually sleeps here
+  sleep_disable();                     // System continues execution here when watchdog timed out
+  sbi(ADCSRA, ADEN);                   // switch Analog to Digitalconverter ON
+
+}
+
 
 // Convert the string in "text" into the
 // series of codes representing the flash
@@ -245,145 +341,55 @@ void convert_text_to_morse(void) {
     }
 
   }
-
-
-};
-
-
-void setup() {
-
-  convert_text_to_morse();
-  pinMode( LED_PIN, OUTPUT );
-  pinMode( ANALOG_PIN, INPUT );
-
-  digitalWrite(LED_PIN, LOW);
-  delay(1000);
-  digitalWrite(LED_PIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_PIN, LOW );
-  delay(250);
-  digitalWrite(LED_PIN, HIGH);
-  delay(250);
-  digitalWrite(LED_PIN, LOW);
-  delay(250);
-  digitalWrite(LED_PIN, HIGH);
-  delay(250);
-  digitalWrite(LED_PIN, LOW);
-  digitalWrite(LED_PIN, HIGH);
-  delay(250);
-  digitalWrite(LED_PIN, LOW);
-  delay(2000);
-
-  setup_watchdog(6); // Set the watchdog for 1s
-
 }
 
-void loop () {
+
+// The code which actually flashes the morse based on the input characters:
+
+void flash_morse() {
 
   int i, j;
   int num_flashes;
   int delay_time;
   int bit_to_read;
 
-  if ( watchdog_counter > WATCHDOG_MAX ) {
-    watchdog_counter = 0;
-
-
-
-    digitalWrite(LED_PIN, LOW);
-    for ( i = 0; i < num_characters; i++ ) {
-      num_flashes = morse[i]  & 0b00001111;
-      if ( num_flashes == SPACE_CODE ) {
-        // Space
-        delay_time = dot_duration * 3;
-        delay( delay_time );
-      }
-      else {
-        bit_to_read = 4;
-        for ( j = 0; j < num_flashes; j++ ) {
-
-          dot_duration = analogRead(ANALOG_PIN );
-          if ( dot_duration < 50)
-            dot_duration = 50;
-
-          if ( bitRead( morse[i], bit_to_read) == DOT ) {
-            delay_time = dot_duration;
-          }
-          else {
-            // Three times a dot for a dash
-            delay_time = 3 * dot_duration;
-          }
-          digitalWrite(LED_PIN, HIGH);
-          delay( delay_time );
-          digitalWrite( LED_PIN, LOW );
-          if ( j != num_flashes - 1 ) {
-            delay( dot_duration ); // One unit delay between flashes
-          }
-          bit_to_read += 1;
-        }
-      }
-      // A delay of 3 units between letters
-      if ( i != num_characters - 1 ) {
-        delay( dot_duration * 3 );
-      }
-
+  for ( i = 0; i < num_characters; i++ ) {
+    num_flashes = morse[i]  & LOW_BIT_MASK;
+    if ( num_flashes == SPACE_CODE ) {
+      // Space
+      delay_time = dot_duration * 3;
+      delay( delay_time );
     }
+    else {
+      bit_to_read = 4;
+      for ( j = 0; j < num_flashes; j++ ) {
 
+        dot_duration = analogRead(ANALOG_PIN );
+        if ( dot_duration < 50)
+          dot_duration = 50;
+
+        if ( bitRead( morse[i], bit_to_read) == DOT ) {
+          delay_time = dot_duration;
+        }
+        else {
+          // Three times a dot for a dash
+          delay_time = 3 * dot_duration;
+        }
+        digitalWrite(LED_PIN, HIGH);
+        delay( delay_time );
+        digitalWrite( LED_PIN, LOW );
+        if ( j != num_flashes - 1 ) {
+          delay( dot_duration ); // One unit delay between flashes
+        }
+        bit_to_read += 1;
+      }
+    }
+    // A delay of 3 units between letters
+    if ( i != num_characters - 1 ) {
+      delay( dot_duration * 3 );
+    }
   }
-
-  system_sleep();
-
 }
-
-
-
-// 0=16ms, 1=32ms,2=64ms,3=128ms,4=250ms,5=500ms
-// 6=1 sec,7=2 sec, 8=4 sec, 9= 8sec
-void setup_watchdog(int ii) {
-
-  byte bb;
-  int ww;
-  if (ii > 9 ) ii = 9;
-  bb = ii & 7;
-  if (ii > 7) bb |= (1 << 5);
-  bb |= (1 << WDCE);
-  ww = bb;
-
-  MCUSR &= ~(1 << WDRF);
-  // start timed sequence
-  WDTCR |= (1 << WDCE) | (1 << WDE);
-  // set new watchdog timeout value
-  WDTCR = bb;
-  WDTCR |= _BV(WDIE);
-}
-
-// Watchdog Interrupt Service / is executed when watchdog timed out
-ISR(WDT_vect) {
-  watchdog_counter++; // increment the global counter
-}
-
-
-// set system into the sleep state
-// system wakes up when wtchdog is timed out
-void system_sleep() {
-
-  cbi(ADCSRA, ADEN);                   // switch Analog to Digitalconverter OFF
-
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN); // sleep mode is set here
-  sleep_enable();
-
-  sleep_mode();                        // System actually sleeps here
-
-  sleep_disable();                     // System continues execution here when watchdog timed out
-
-  sbi(ADCSRA, ADEN);                   // switch Analog to Digitalconverter ON
-
-}
-
-
-
-
-
 
 
 
